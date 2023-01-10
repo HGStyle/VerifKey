bfc5fd8719012107d8d539c34ae4c3b4b36a0154
HGStyle <84084268+HGStyle@users.noreply.github.com>
2023-01-10 19:38:45 +0100

    Use this file to install VerifKey.

---
+++ VerifKey_installer.py
@@ -0,0 +1,32 @@
+import shutil, os
+
+print('Starting installation of VerifKey...')
+print('This will only take a second...')
+
+# Get the Python lib folder
+libdir = os.sep.join(shutil.__file__.split(os.sep)[:-1])
+if not libdir.endswith(os.sep):
+	libdir += os.sep
+
+# Try downloading VerifKey with `requests` module or `urllib` module
+try:
+	import urllib.request
+	r = urllib.request.urlopen('https://raw.githubusercontent.com/HGStyle/VerifKey/main/VerifKey.py')
+	with open('VerifKey.py', 'wb') as f:
+		f.write(r.readlines()[0])
+except ImportError:
+	import requests
+	r = requests.get('https://raw.githubusercontent.com/HGStyle/VerifKey/main/VerifKey.py')
+	with open('VerifKey.py', 'wb') as f:
+		f.write(r.content)
+
+# Install VerifKey
+try:
+	shutil.move('VerifKey.py', libdir + 'VerifKey.py')
+except PermissionError:
+	print('Please run this programm as administrator/root !')
+	os.remove('VerifKey.py')
+	exit(1)
+
+print('VerifKey module is now installed !')
+print('Note that VerifKey is only installed for this Python version !')
